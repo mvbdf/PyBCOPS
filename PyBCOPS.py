9ec3f7f141b5468bb043ae63fc25824135317c5d
Matheus Barreto <56487206+mvbdf@users.noreply.github.com>
2023-07-20 09:36:36 -0300

    Add files via upload

--- PyBCOPS.py
+++ PyBCOPS.py
@@ -2,19 +2,19 @@ import numpy as np
 import pandas as pd
 
 
-def conformalscore(ste, s):
-    """ Adaptado de conformalscore.cpp """
-    return [np.sum(ste[i] >= s) for i in range(len(ste))]
+def conformalscore(s_test, s_train):
+    """ Adapted from conformalscore.cpp """
+    return [np.sum(s_test[i] >= s_train) for i in range(len(s_test))]
 
 
-def conformal_scores(ste, s, y, labels):
+def conformal_scores(s_test, s_train, y_train, labels):
     K = len(labels)
-    prediction_te = np.zeros((np.shape(ste)[0], K))
+    prediction_te = np.zeros((np.shape(s_test)[0], K))
 
     for k in range(K):
-        sk = s[y == labels[k], k]
+        sk = s_train[y_train == labels[k], k]
 
-        prediction_te[:,k] = conformalscore(ste[:,k], sk)
+        prediction_te[:,k] = conformalscore(s_test[:,k], sk)
         prediction_te[:,k] = (prediction_te[:,k] + 1) / (len(sk) + 1)
     
     return prediction_te
@@ -57,16 +57,32 @@ def prediction(models, x_train, y_train, labels, x_test):
     return {'prediction_conformal':prediction_conformal, 'score_test':ste, 'score_train':s}
 
 
-def BCOPS(classifier, x_train1, y_train1, x_train2, y_train, x_test1, x_test2,
-          labels, *classifier_args, **classifier_kargs):
+def BCOPS(classifier, X_train, y_train, X_test, *classifier_args, **classifier_kargs):
+    # Data-split
+    foldid = np.random.randint(1, 3, len(y_train))
+    foldid_te = np.random.randint(1, 3, np.shape(X_test)[0])
+
+    x_train1 = X_train[foldid==1,:]
+    x_train2 = X_train[foldid==2,:]
+    y_train1 = y_train[foldid==1]
+    y_train2 = y_train[foldid==2]
+    x_test1 = X_test[foldid_te ==1,:]
+    x_test2 = X_test[foldid_te==2,:]
+    labels = np.unique(y_train)
+
     # Training
-    models1 = train(classifier, x_train2, y_train, labels, x_test2, *classifier_args, **classifier_kargs)
+    models1 = train(classifier, x_train2, y_train2, labels, x_test2, *classifier_args, **classifier_kargs)
     models2 = train(classifier, x_train1, y_train1, labels, x_test1, *classifier_args, **classifier_kargs)
     
     # Prediction
     prediction1 = prediction(models1, x_train1, y_train1, labels, x_test1)['prediction_conformal']
-    prediction2 = prediction(models2, x_train2, y_train, labels, x_test2)['prediction_conformal']
-    return {'conformal_scores1':prediction1, 'conformal_scores2':prediction2}
+    prediction2 = prediction(models2, x_train2, y_train2, labels, x_test2)['prediction_conformal']
+
+    prediction_conformal = np.zeros((np.shape(X_test)[0], len(labels)))
+    prediction_conformal[foldid_te==1,:] = prediction1
+    prediction_conformal[foldid_te==2,:] = prediction2
+
+    return prediction_conformal
 
 
 def evaluate_conformal(prediction, y_test, labels, alpha=0.05):
@@ -81,3 +97,47 @@ def evaluate_conformal(prediction, y_test, labels, alpha=0.05):
     res.columns = labels
     res.index = labels_te
     return res
+
+def test():
+    from sklearn.ensemble import RandomForestClassifier
+
+    np.random.seed(123)
+
+    X_train = np.zeros((1000, 10))
+    y_train = np.zeros(1000, dtype = int)
+
+    X_test = np.zeros((1500, 10))
+    y_test = np.zeros(1500, dtype = int)
+
+    for i in range(500):
+        ## Dados para o treino
+        # Classe 0
+        X_train[i,:] = np.random.normal(0, 1, 10)
+        y_train[i] = 0
+        
+        # Classe 1
+        X_train[i + 500,] = np.concatenate([np.random.normal(3, 0.5, 1), np.random.normal(0, 1, 9)])
+        y_train[i + 500] = 1
+        
+        ## Dados para o teste
+        # Classe 0
+        X_test[i,:] = np.random.normal(0, 1, 10)
+        y_test[i] = 0
+        
+        # Classe 1
+        X_test[i + 500,:] = np.concatenate([np.random.normal(3, 0.5, 1), np.random.normal(0, 1, 9)])
+        y_test[i + 500] = 1
+        
+        # Classe 2 (outliers)
+        X_test[i + 1000,:] = np.concatenate([np.random.normal(0, 1, 1),
+                                            np.random.normal(3, 0.5, 1),
+                                            np.random.normal(0, 1, 8)])
+        y_test[i + 1000] = 2
+
+    prediction_conformal = BCOPS(RandomForestClassifier, X_train, y_train, X_test)
+
+    evaluation = evaluate_conformal(prediction_conformal, y_test, np.unique(y_train))
+    print(evaluation)
+
+if __name__ == '__main__':
+    test()
