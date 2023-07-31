d31fbc098d5b29ab39ab3455deb4fb01a70d1ce8
mvbdf <mvbdef@gmail.com>
2023-07-31 13:52:30 -0300

    calculate conformal score using C library

---
+++ conformalscore.c
@@ -0,0 +1,6 @@
+void conformalscore(float *ste, float *s, int* count, int size_ste, int size_s) {
+    for(int i = 0; i < size_ste; i++)
+        for(int j = 0; j < size_s; j++)
+            if(ste[i] >= s[j])
+                count[i]++;
+}
