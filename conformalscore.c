fcd2fab7ab043c86110fe914fb81d4b150219985
mvbdf <mvbdef@gmail.com>
2023-07-31 14:19:45 -0300

    include compilation command

--- conformalscore.c
+++ conformalscore.c
@@ -1,3 +1,5 @@
+// use: gcc -fpic -shared -o libscore.so conformalscore.c
+
 void conformalscore(float *ste, float *s, int* count, int size_ste, int size_s) {
     for(int i = 0; i < size_ste; i++)
         for(int j = 0; j < size_s; j++)
